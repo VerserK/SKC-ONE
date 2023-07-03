3727952e911741e33858021c0758f7a2894e4ae9
akarawatp <akarawat.p@siamkubota.co.th>
2023-07-03 15:17:18 +0700

    dqwdqwdqwd

--- main.py
+++ main.py
@@ -2,38 +2,14 @@ from fastapi import FastAPI, Form, Request, status
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import uvicorn
-
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-# @app.get("/", response_class=HTMLResponse)
-# async def index(request: Request):
-#     print('Request for index page received')
-#     return templates.TemplateResponse('index.html', {"request": request})
-
-@app.get("/", response_class=HTMLResponse)
-async def index():
+@app.get('/')
+def index():
     hello = 'Hello World!'
     print('Request for index page received')
     return hello
-
-@app.get('/favicon.ico')
-async def favicon():
-    file_name = 'favicon.ico'
-    file_path = './static/' + file_name
-    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
-
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 
 if __name__ == '__main__':
     app.run()
